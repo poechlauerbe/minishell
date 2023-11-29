ed71fb80f0d22ef09f13330d2e358909d93e1d9f
Thorben Benz <tbenz@c2r4p9.42vienna.com>
2023-11-29 17:45:01 +0100

    changes to the signal handling, added an exit function and a first implementation of pwd

--- sources/signal_handling.c
+++ sources/signal_handling.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_handle_signals.c                                :+:      :+:    :+:   */
+/*   signal_handling.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 14:56:01 by tbenz             #+#    #+#             */
-/*   Updated: 2023/11/29 14:03:59 by tbenz            ###   ########.fr       */
+/*   Updated: 2023/11/29 16:52:44 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,12 @@ void	ft_handler_s(int signum, siginfo_t *info, void *no)
 	infos = info;
 	no = NULL;
 	if (signum == SIGINT)
-	/* if there is a foreground process running, the process will send the
-	process a SIGINT, otherwise a new prompt will be issued */
-		exit(128 + SIGINT);
+	{
+		rl_replace_line("", 0);
+		write(1, "\n", 1);
+		rl_on_new_line();
+		rl_redisplay();
+	}
 }
 
 void	ft_handle_singals(void)
@@ -33,7 +36,6 @@ void	ft_handle_singals(void)
 	sigemptyset(&act_int.sa_mask);
 	act_int.sa_flags = SA_SIGINFO;
 	sigaction(SIGINT, &act_int, NULL);
-
 	act_quit.sa_sigaction = (void *)SIG_IGN;
 	sigemptyset(&act_quit.sa_mask);
 	act_quit.sa_flags = 0;
