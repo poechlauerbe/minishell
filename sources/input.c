a14a05a9c08e8df31c5614ffd48bebd7e0fa4faa
Benjamin Pochlauer <bpochlau@c2r4p8.42vienna.com>
2023-12-13 18:42:14 +0100

    programs split by a input or output file is working

--- sources/input.c
+++ sources/input.c
@@ -6,7 +6,7 @@
 /*   By: bpochlau <bpochlau@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 15:30:48 by bpochlau          #+#    #+#             */
-/*   Updated: 2023/12/13 17:11:06 by bpochlau         ###   ########.fr       */
+/*   Updated: 2023/12/13 18:38:36 by bpochlau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -174,29 +174,86 @@ void	ft_red_file(t_vars *vars, t_prg *lst)
 	}
 }
 
+void	ft_check_prog_len(int *count, t_prg *p_prog, t_prg *prog)
+{
+	char	**temp;
+
+	temp = p_prog->prog;
+	while (*temp)
+	{
+		*count += 1;
+		temp += 1;
+	}
+	temp = prog->prog;
+	while (*temp)
+	{
+		*count += 1;
+		temp += 1;
+	}
+}
+
+void	ft_transfer_prog_str(char **new_prog, char **p_prog, char **prog)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (p_prog[i])
+	{
+		new_prog[i] = p_prog[i];
+		i++;
+	}
+	j = 0;
+	while (prog[j])
+	{
+		new_prog[i] = prog[j];
+		i++;
+		j++;
+	}
+}
+
 void	ft_combine(t_vars *vars, t_prg *prog, t_prg *p_prog, t_prg *last)
 {
+	int		count;
+	char	**new_prog;
 
+	count = 0;
+	ft_check_prog_len( &count, p_prog, prog);
+	new_prog = malloc((count + 1) * sizeof(char *));
+	if (!new_prog)
+		ft_exit(vars, MALLOC_ERROR);
+	new_prog[count] = NULL;
+	ft_transfer_prog_str(new_prog, p_prog->prog, prog->prog);
+	if (p_prog->prog)
+		free(p_prog->prog);
+	p_prog->prog = new_prog;
+	last->next = prog->next;
+	if (prog->prog)
+		free(prog->prog);
+	free(prog);
 }
 
 void ft_comb_progs(t_vars *vars)
 {
 	t_prg	*temp;
-	t_prg	*p_temp;
+	t_prg	*p_last;
 	t_prg	*last;
 	int		i;
 
 	i = 0;
 	temp = vars->p_start;
+	p_last = vars->p_start;
 	while (temp)
 	{
-		if (temp->oper == '0' && i != 0)
+		if (temp->oper == '0')
 		{
-			ft_combine(vars, temp, p_temp, last);
+			if (i != 0)
+				ft_combine(vars, temp, p_last, last);
 			i++;
 		}
 		if (temp->oper == '|')
-			p_temp = temp;
+			p_last = temp;
 		last = temp;
 		temp = temp->next;
 	}
--- sources/testbenny.c
+++ sources/testbenny.c
@@ -6,7 +6,7 @@
 /*   By: bpochlau <bpochlau@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 10:17:45 by bpochlau          #+#    #+#             */
-/*   Updated: 2023/12/12 15:01:45 by bpochlau         ###   ########.fr       */
+/*   Updated: 2023/12/13 18:39:58 by bpochlau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,23 +60,23 @@ int main(int argc, char *argv[])
 	vars.inp = argv[1];
 	vars.exit_code = 99;
 	ft_check_input(&vars);
-	ft_pipe_loop(&vars);
-	// temp = vars.p_start;
-	// while (temp)
-	// {
-	// 	printf("oper: %c$\n", temp->oper);
-	// 	i = -1;
-	// 	while (temp->prog[++i])
-	// 		printf("string[%i]: %s\n", i, temp->prog[i]);
-	// 	printf("\n");
-	// 	// while(temp->in_file)
-	// 	// {
-	// 	// 	printf("in: %s$\n", temp->in_file->file);
-	// 	// 	temp->in_file = temp->in_file->next;
-	// 	// }
-	// 	temp = temp->next;
-	// }
-	// ft_exit(&vars, OK);
+	// ft_pipe_loop(&vars);
+	temp = vars.p_start;
+	while (temp)
+	{
+		printf("oper: %c$\n", temp->oper);
+		i = -1;
+		while (temp->prog[++i])
+			printf("string[%i]: %s\n", i, temp->prog[i]);
+		printf("\n");
+		// while(temp->in_file)
+		// {
+		// 	printf("in: %s$\n", temp->in_file->file);
+		// 	temp->in_file = temp->in_file->next;
+		// }
+		temp = temp->next;
+	}
+	ft_exit(&vars, OK);
 }
 
 // int	main(void)
