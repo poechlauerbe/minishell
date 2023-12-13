55e13d3acfcd4c69275280279241cd5c4c6bc96d
Benjamin Pochlauer <bpochlau@c2r4p8.42vienna.com>
2023-12-13 12:30:06 +0100

    exitcodes working

--- sources/expand.c
+++ sources/expand.c
@@ -6,7 +6,7 @@
 /*   By: bpochlau <bpochlau@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 12:15:16 by bpochlau          #+#    #+#             */
-/*   Updated: 2023/12/12 14:59:37 by bpochlau         ###   ########.fr       */
+/*   Updated: 2023/12/12 17:36:43 by bpochlau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,9 @@ void	ft_expander(t_vars *vars, char **arg, t_quote *quote)
 		ft_expand_str(vars, arg, quote, temp);
 		free(temp);
 	}
+	else if (str[i] == ' ')
+		;
+	
 }
 
 void	ft_expand_all_vars(t_vars *vars)
