c5826ad03ba1dd06681e19540c64c0aeeb692591
Benjamin Pochlauer <bpochlau@c2r4p8.42vienna.com>
2023-12-14 11:14:26 +0100

    added additional file

---
+++ libraries/libft/b_exit.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   b_exit.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpochlau <bpochlau@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 11:12:25 by bpochlau          #+#    #+#             */
+/*   Updated: 2023/12/14 11:13:08 by bpochlau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	num;
+	int	pre;
+
+	pre = 1;
+	num = 0;
+	i = 0;
+	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t'
+		|| nptr[i] == '\r' || nptr[i] == '\f' || nptr[i] == '\v')
+		i++;
+	if (nptr[i] == '-')
+	{
+		pre = -1;
+		i++;
+	}
+	else if (nptr[i] == '+')
+		i++;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+		num = 10 * num + nptr[i++] - 48;
+	return (num * pre);
+}
