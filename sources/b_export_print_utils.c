6e78b0540ad20f3e1b1cf622591f298e4e11187d
Thorben Benz <tbenz@c2r4p9.42vienna.com>
2024-01-02 19:08:46 +0100

    added error printer for invalid export key which sets the exit code as well

--- sources/b_export_print_utils.c
+++ sources/b_export_print_utils.c
@@ -6,7 +6,7 @@
 /*   By: tbenz <tbenz@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 14:36:23 by tbenz             #+#    #+#             */
-/*   Updated: 2023/12/09 15:30:27 by tbenz            ###   ########.fr       */
+/*   Updated: 2024/01/02 19:04:33 by tbenz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,25 @@ void	ft_set_ptr(t_vars *vars, t_kv **elem, t_kv *prev)
 		(*elem)->nxtao = next;
 		next->prev = (*elem);
 	}
+}
+
+void	ft_key_err_print(t_vars *vars, char *key, char *val)
+{
+	char	*tmp1;
+	char	*tmp2;
+
+	tmp1 = ft_strjoin("minishell: export: `", key);
+	if (!tmp1)
+		ft_exit(vars, MALLOC_ERROR);
+	tmp2 = ft_strjoin(tmp1, val);
+	free(tmp1);
+	if (!tmp2)
+		ft_exit(vars, MALLOC_ERROR);
+	tmp1 = ft_strjoin(tmp2, "': not a valid identifier\n");
+	free(tmp2);
+	if (!tmp1)
+		ft_exit(vars, MALLOC_ERROR);
+	ft_printf_fd(2, tmp1);
+	free (tmp1);
+	vars->exit_code = 1;
 }
