8b24b89f12be56cbfd0ca1bfd9e351f19d226448
Benjamin Pochlauer <bpochlau@c4r7p10.42vienna.com>
2024-01-16 14:30:25 +0100

    norm of progs

--- sources/prog.c
+++ sources/prog.c
@@ -6,7 +6,7 @@
 /*   By: bpochlau <bpochlau@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 13:41:52 by bpochlau          #+#    #+#             */
-/*   Updated: 2024/01/16 14:01:13 by bpochlau         ###   ########.fr       */
+/*   Updated: 2024/01/16 14:24:15 by bpochlau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,20 +38,11 @@ int	ft_builtin_check(t_vars *vars, t_prg *prog)
 	return (USED);
 }
 
-void	ft_check_prog(t_vars *vars, t_prg *prog)
+void	ft_check_w_stand_path(t_vars *vars, t_prg *prog)
 {
 	int		acc_c;
 
 	acc_c = 1;
-	if (!prog->prog || !prog->prog[0])
-		return ;
-	if (prog->prog[0][0] == '\0')
-	{
-		ft_putstr_fd("Command '' not found\n", 2);
-		vars->no_exec = 127;
-		vars->exit_code = 127;
-		ft_exit(vars, 127);
-	}
 	if (ft_strncmp(prog->prog[0], "./", 2) == 0)
 		acc_c = access(prog->prog[0], F_OK | X_OK);
 	else if (ft_strncmp(prog->prog[0], "../", 3) == 0)
@@ -66,6 +57,20 @@ void	ft_check_prog(t_vars *vars, t_prg *prog)
 	}
 	else if (acc_c == OK)
 		execve(prog->prog[0], prog->prog, vars->envp);
+}
+
+void	ft_check_prog(t_vars *vars, t_prg *prog)
+{
+	if (!prog->prog || !prog->prog[0])
+		return ;
+	if (prog->prog[0][0] == '\0')
+	{
+		ft_putstr_fd("Command '' not found\n", 2);
+		vars->no_exec = 127;
+		vars->exit_code = 127;
+		ft_exit(vars, 127);
+	}
+	ft_check_w_stand_path();
 	if (ft_builtin_check(vars, prog) == USED)
 		ft_exit(vars, OK);
 	ft_check_path(vars, prog);
